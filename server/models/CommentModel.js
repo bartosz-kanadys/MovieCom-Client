dc90f19ac50f1cb7b71d69acb83dea7b0abecd01
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-24 21:37:43 +0200

    Update User and Comments model

--- server/models/CommentModel.js
+++ server/models/CommentModel.js
@@ -10,7 +10,7 @@ const commentSchema = new mongoose.Schema({
         required: true
     },
     content: {
-        type: Date,
+        type: String,
         required: true
     }
   });
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    login: String,
-    email: String,
-    password: String,
-    role: [String]
-  });
+  login: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: [String],
+    default: ['user']
+  }
+});
 
 const User = mongoose.model('user', userSchema);
 module.exports = { User }
