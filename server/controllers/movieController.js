143a4cd58d6d31532a80cacf2b060d37c0dd31a2
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-24 17:29:18 +0200

    Update movie API

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -24,14 +24,36 @@ module.exports = {
         }
     },
 
-    async create(req, res) {
+    async getByTitle(req, res) {
         try {
-            let movie =  new Movie(req.body)
-            await movie.save()
+            const movie = await Movie.find({title: req.params.title})
             res.status(200).json(movie)
 
         } catch (error) {
             res.status(500).json({error: "Internal error server"})
         }
-    }
+    },
+
+    async create(req, res) {
+        try {
+            let movie =  new Movie(req.body)
+            await movie.save()
+            res.status(200).json({msg: "Dodano do bazy"})
+
+        } catch (error) {
+            res.status(500).json({error: "Internal error server"})
+        }
+    },
+
+    async update(req, res) {
+        try {
+            const filter = req.body.filter
+            const update = req.body.update
+            console.log(filter)
+            await Movie.findOneAndUpdate(filter, update)
+            res.status(200).json({msg: "Updated"})
+        } catch (error) {
+            res.status(500).json({error: "Internal error server"})
+        }
+    },
 }
--- server/routes/moviesRoute.js
+++ server/routes/moviesRoute.js
@@ -2,11 +2,17 @@ var express = require("express");
 const movieController = require("../controllers/movieController");
 var router = express.Router();
 
-
+//GET - READ
 router.get("/", movieController.getAll);
+router.get('/id/:id', movieController.getById)
+router.get('/title/:title', movieController.getByTitle)
 
-router.get('/:id', movieController.getById)
-
+//POST - CREATE
 router.post('/insert', movieController.create)
+
+//UPDATE
+router.put('/update', movieController.update)
+
+//DELETE
 
 module.exports = router;
