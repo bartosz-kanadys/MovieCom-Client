8c1c31e985fbcd3df6ba2847fc4e43e88ee5ad05
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-25 20:39:27 +0200

    Add screen media to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,24 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '100px',
+      // => @media (min-width: 100px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '810px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1040px',
+      // => @media (min-width: 1040px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
