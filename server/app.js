ffc11e478199aa63c82d70a9dc74d30ce97833cb
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-25 20:37:58 +0200

    Update cors on the server

--- server/app.js
+++ server/app.js
@@ -21,12 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({
+  origin: 'http://localhost:5173' // Zmień na adres swojej aplikacji frontendowej
+}));
 app.use('/', authorizationRouter);
 app.use('/users', usersRouter);
 app.use("/movies", moviesRouter);
 app.use('/comments', commentRouter)
-app.use(cors())
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
