f6c486a5a14c955355a53b90eec1f0928113b622
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-28 21:52:13 +0200

    Update MoviePage

--- client/src/components/MoviePage/MoviePage.jsx
+++ client/src/components/MoviePage/MoviePage.jsx
@@ -3,32 +3,21 @@ import Header from '../Header/Header'
 import { useParams } from 'react-router-dom';
 import '../MovieCard/movieCard.css'
 import MoviePanel from './MoviePanel';
+import Comment from './Comment';
+import CommentsPanel from './CommentsPanel';
 
 
 
 function MoviePage() {
-    // const [movie, setMovie] = useState({})
 
-     let { id } = useParams();
+    let { id } = useParams();
 
-    // useEffect(()=>{
-    //     fetchData()
-    // },[])
-
-    // const fetchData = () => {
-    //     const fetchData = async () => {
-    //         const response = await axios.get(`http://localhost:9000/movies/id/${id}`)
-    //         setMovie(response.data)
-    //     }
-
-    //     fetchData()
-    // }
     return (
         <>
-        <Header isSearch={false}></Header>
-        
-        <MoviePanel id={id}></MoviePanel>
-        
+            <Header isSearch={false}></Header>
+
+            <MoviePanel id={id}></MoviePanel>
+            <CommentsPanel id={id}></CommentsPanel>
         </>
     )
 }
